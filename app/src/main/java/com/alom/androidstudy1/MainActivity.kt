45c01f34dcfc280c7e3fa9119e50eca9a64d9762
ihb0301 <abin0301@naver.com>
2024-09-29 12:59:59 +0900

    feat: StateFlow와 SharedPreferences 이용한 저장 기능 추가

--- app/src/main/java/com/alom/androidstudy1/MainActivity.kt
+++ app/src/main/java/com/alom/androidstudy1/MainActivity.kt
@@ -3,21 +3,27 @@ package com.alom.androidstudy1
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.alom.androidstudy1.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(),OnClickListener {
     private lateinit var memoViewModel : MemoViewModel
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         //뷰바인딩 적용
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -31,9 +37,27 @@ class MainActivity : AppCompatActivity(),OnClickListener {
 
         //버튼 리스너 설정
         binding.button.setOnClickListener(this)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    memoViewModel.currentText.collect{
+                        //EditText Text를 SharedPreferences에서 불러온 글자로 변경
+                        binding.editTextTextMultiLine.setText(it)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        memoViewModel.returnMemo(applicationContext)
     }
 
     override fun onClick(view: View?) {
         //SharedPreference 저장 함수 호출
+        memoViewModel.saveMemo(binding.editTextTextMultiLine.text.toString(),applicationContext)
     }
 }
--- app/src/main/java/com/alom/androidstudy1/MemoViewModel.kt
+++ app/src/main/java/com/alom/androidstudy1/MemoViewModel.kt
@@ -1,8 +1,28 @@
 package com.alom.androidstudy1
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MemoViewModel : ViewModel(){
-    //Todo.SharedPreference 저장 함수 구현
+    val _currentText = MutableStateFlow<String>("")
+    val currentText:StateFlow<String> = _currentText
+
+    fun returnMemo(context: Context){
+        val pref:SharedPreferences = context.getSharedPreferences("pref",Context.MODE_PRIVATE)
+        _currentText.value = pref.getString("content","")!!
+    }
+
+    fun saveMemo(content:String,context: Context){
+        //Todo.SharedPreference 저장 구현
+        _currentText.value = content
+
+        val pref:SharedPreferences = context.getSharedPreferences("pref",Context.MODE_PRIVATE)
+        pref.edit().putString("content",content).apply()
+
+        Toast.makeText(context,"메모가 저장되었습니다",Toast.LENGTH_LONG).show()
+    }
 }
