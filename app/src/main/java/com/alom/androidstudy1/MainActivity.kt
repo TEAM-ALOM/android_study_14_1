41c9125fface4cb9a029a5f02d3b6f309b2dc249
ihb0301 <abin0301@naver.com>
2024-09-29 11:35:25 +0900

    feat: MVVM 구조 적용

--- app/src/main/java/com/alom/androidstudy1/MainActivity.kt
+++ app/src/main/java/com/alom/androidstudy1/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.alom.androidstudy1
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.alom.androidstudy1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),OnClickListener {
+    private lateinit var memoViewModel : MemoViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,5 +25,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //ViewModel
+        memoViewModel = ViewModelProvider(this).get(MemoViewModel::class.java)
+
+        //버튼 리스너 설정
+        binding.button.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        //SharedPreference 저장 함수 호출
     }
 }
---
+++ app/src/main/java/com/alom/androidstudy1/MemoViewModel.kt
@@ -0,0 +1,8 @@
+package com.alom.androidstudy1
+
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+
+class MemoViewModel : ViewModel(){
+    //Todo.SharedPreference 저장 함수 구현
+}
