73fa9e5cfc7120e73570affeba513af61901fe13
hwan04 <73435826+hwan04@users.noreply.github.com>
2024-10-06 23:04:46 +0900

    feat(MainActivity): 버튼 클릭 이벤트

--- app/src/main/java/com/alom/androidstudy1/MainActivity.kt
+++ app/src/main/java/com/alom/androidstudy1/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.alom.androidstudy1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var memoViewModel: MemoViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        memoViewModel = ViewModelProvider(this)[MemoViewModel::class.java]
 
+        binding.btnSave.setOnClickListener{
+            memoViewModel.updateMemo(binding.etText.text.toString())
+        }
     }
 }
