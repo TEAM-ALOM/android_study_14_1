bab493dac514008dd3a1694392adc63f11170530
hwan04 <73435826+hwan04@users.noreply.github.com>
2024-10-06 23:28:31 +0900

    fix(ViewModel): Context 못 받아오는 오류 해결

--- app/src/main/java/com/alom/androidstudy1/MemoViewModel.kt
+++ app/src/main/java/com/alom/androidstudy1/MemoViewModel.kt
@@ -1,18 +1,24 @@
 package com.alom.androidstudy1
 
+import android.app.Application
 import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class MemoViewModel(context: Context) : ViewModel() {
+class MemoViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val preferenceUtil = PreferenceUtil(context)
+    private val preferenceUtil = PreferenceUtil(getApplication<Application>().applicationContext)
     private val _currentValue = MutableStateFlow<String>("")
 
     val currentValue: StateFlow<String> = _currentValue
+
+    init{
+        _currentValue.value = preferenceUtil.getString("memo", "")
+    }
 
     fun updateMemo(newMemo: String) {
         _currentValue.value = newMemo
