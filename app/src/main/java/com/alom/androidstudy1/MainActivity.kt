dfa425e7dc92afbf1f0d2beac36988f2d19f5a3e
MisterJerry <misterjerry@sju.ac.kr>
2024-10-10 23:58:44 +0900

    refactor: Replace LiveData with StateFlow

--- app/src/main/java/com/alom/androidstudy1/MainActivity.kt
+++ app/src/main/java/com/alom/androidstudy1/MainActivity.kt
@@ -2,14 +2,13 @@ package com.alom.androidstudy1
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.alom.androidstudy1.databinding.ActivityMainBinding
-import com.alom.androidstudy1.viewModel.MemoViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var memoViewModel: MemoViewModel
@@ -24,18 +23,26 @@ class MainActivity : AppCompatActivity() {
         memoViewModel = ViewModelProvider(this).get(MemoViewModel::class.java)
 
         val sharedPreferences = getSharedPreferences("memo", MODE_PRIVATE)
-
+        val editor = sharedPreferences.edit()
         Log.d(TAG, sharedPreferences.getString("memo","").toString())
         val initData = sharedPreferences.getString("memo","").toString()
+
         binding.etMemo.setText(initData)
         Log.d(TAG+"textview", binding.etMemo.text.toString())
 
-        memoViewModel.currentValue.observe(this, Observer {
-            binding.etMemo.setText(it.toString())
-        })
-        val btnSave = binding.btnSave
-        btnSave.setOnClickListener {
-            memoViewModel.saveMemo(this,binding.etMemo.text.toString())
+        lifecycleScope.launch{
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                launch {
+                    memoViewModel.currentValue.collect{
+                        editor.putString("memo",it)
+                        editor.apply()
+                    }
+                }
+            }
+        }
+
+        binding.btnSave.setOnClickListener {
+            memoViewModel.saveMemo(binding.etMemo.text.toString())
         }
     }
 }
